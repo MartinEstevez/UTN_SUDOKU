529b6d0e2ab360a00366379b3520693388fe84e1
MartinEstevez <martin.2003estevez@gmail.com>
2024-11-29 14:02:24 -0300

    Se modifica la pantalla de puntajes, se agrega el boton de menu en distintas pantallas y se optimiza el codigo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from Biblioteca_funciones import *
+from puntajes import *
 
 
 pygame.init() #inicializo pygame 
@@ -7,16 +8,27 @@ pygame.mixer.init() #inicializo musica
 
 pantalla_actual = "Inicio"
 
-pantalla = inicializar_caracteristicas_pantalla(1534, 801, "Super Sudoku","Segundo_parcial\icono_ventanita.jpg")
+pantalla = inicializar_caracteristicas_pantalla(1534, 801, "Super Sudoku","icono_ventanita.jpg")
 
 actualizar_pantalla = True #botones titilando
 ultimo_etado_del_mouse = None #botones titilando
 
+contador_inicio = pygame.time.get_ticks()  # Tiempo inicial para el contador
+fuente = pygame.font.SysFont("Arial", 30)  # Fuente para el contador
 
-ruta_imagen_inicio = "Segundo_parcial/pantalla_inicio.png"
-ruta_imagen_seleccion_niveles = "Segundo_parcial/pantalla_eleccion_nivel.png"
+#posicion_tablero = (400, 100)  # Posición (x, y) del tablero en la pantalla
+#tamanio_tablero = (540, 540)  # Tamaño del tablero (ancho, alto)
 
-pygame.mixer.music.load("Segundo_parcial/musica_inicio.mp3") #sonido largo de fondo
+colores_nivel_actual = {
+    "fondo": (0, 0, 0),  # Negro
+    "celdas": (255, 255, 255),  # Blanco
+}
+
+ruta_imagen_inicio = "pantalla_inicio.png"
+ruta_imagen_seleccion_niveles = "pantalla_eleccion_nivel.png"
+ruta_imagen_puntajes = "puntajes.png"
+
+pygame.mixer.music.load("musica_inicio.mp3") #sonido largo de fondo
 pygame.mixer.music.set_volume(0.4)
 pygame.mixer.music.play(loops=-1, start=0.0)
 
@@ -50,13 +62,15 @@ while corriendo == True:
                     pantalla_actual = "Seleccion niveles"
                     actualizar_pantalla = True
                 elif accion == "Puntajes":
-                    pass #muestra puntajes
+                    pantalla_actual = "Puntajes"
+                    actualizar_pantalla = True #muestra puntajes
                 elif accion == "Salir": 
                     corriendo = False
             
             if actualizar_pantalla: 
                 mostrar_pantalla_inicio(pantalla, ruta_imagen_inicio)
                 actualizar_pantalla = False
+
 
         elif pantalla_actual == "Seleccion niveles":
             mostrar_pantalla_niveles(pantalla, ruta_imagen_seleccion_niveles)
@@ -69,13 +83,15 @@ while corriendo == True:
                     pantalla_actual = "Facil"
                     cargar_fondo_y_musica_segun_nivel(pantalla, "Facil")
                     colores_nivel_actual = asignar_colores_sudoku_segun_nivel("Facil")
+                    mostrar_boton_menu(pantalla)
                     actualizar_pantalla = True
                     pygame.mixer.music.stop() #SACAR ESTO
-                    
+
                 elif nivel == "Medio":
                     pantalla_actual = "Medio"
                     cargar_fondo_y_musica_segun_nivel(pantalla, "Medio")
                     colores_nivel_actual =  asignar_colores_sudoku_segun_nivel("Medio")
+                    mostrar_boton_menu(pantalla)
                     actualizar_pantalla = True
                     pygame.mixer.music.stop() #SACAR ESTO
 
@@ -83,21 +99,32 @@ while corriendo == True:
                     pantalla_actual = "Dificil"
                     cargar_fondo_y_musica_segun_nivel(pantalla, "Dificil")
                     colores_nivel_actual = asignar_colores_sudoku_segun_nivel("Dificil")
+                    mostrar_boton_menu(pantalla)
                     actualizar_pantalla = True
                     pygame.mixer.music.stop() #SACAR ESTO
-
+                    
                 mostrar_sudoku(pantalla, colores_nivel_actual["lineas"][0], colores_nivel_actual["lineas"][1], colores_nivel_actual["lineas"][2], colores_nivel_actual["fondo"])
-                # mostrar_numeros_dentro_sudoku(pantalla, funciondeltablero)
+                #mostrar_numeros_dentro_sudoku(pantalla, funciondeltablero)
 
+                texto, rect = iniciar_contador(contador_inicio, fuente, (1400, 50), (0, 0, 0))
+                pantalla.blit(texto, rect)
+
+            pygame.display.flip() #actualiza pmatlla 
+
+        
         # elif pantalla_actual == "Medio":   
         #     pass
         # elif pantalla_actual == "Dificil":
         #     pass
-    
-    
-    
-    
-    
+        elif pantalla_actual == "Puntajes":
+            mostrar_pantalla_puntajes_jugadores(pantalla, ruta_imagen_puntajes)
+            mostrar_puntajes(pantalla, puntajes)
+            mostrar_boton_menu(pantalla)
+
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                pantalla_actual = "Inicio"
+                actualizar_pantalla = True
     
     # pantalla.fill((60,15,0)) #color de fondo 
 
--- puntajes.py
+++ puntajes.py
@@ -31,13 +31,13 @@ def mostrar_puntajes(pantalla, puntajes):
     color_texto = (0, 0, 0)
 
     x, y = 600, 300
-    espacio_vertical = 100
+    espaciado = 100
 
     for i, entrada in enumerate(puntajes):
         texto = f"{i + 1}. {entrada.get('nombre', ' ')}: {entrada.get('puntaje', )}"
         texto_renderizado = fuente.render(texto, True, color_texto)
         pantalla.blit(texto_renderizado, (x, y))
-        y += espacio_vertical
+        y += espaciado
 
 puntajes = cargar_puntajes("puntajes.json")
 print(puntajes)
@@ -52,22 +52,8 @@ def mostrar_pantalla_puntajes_jugadores(pantalla:pygame.Surface, ruta_imagen:str
     imagen_ranking = pygame.image.load(ruta_imagen)
     imagen_ranking = pygame.transform.scale(imagen_ranking, pantalla.get_size())
     pantalla.blit(imagen_ranking, (0, 0))
-    
-    x_menu = 750
-    y_menu = 700
-
-    fuente_30 = pygame.font.SysFont("Arial", 40)
-    texto_menu = fuente_30.render("MENU", True, "#000000")
-    boton_menu = texto_menu.get_rect(center=(x_menu, y_menu))
-    menu = pygame.Rect.inflate(boton_menu, 10, 10)
-    pygame.draw.rect(pantalla, "#000000", menu, 3)
-
-    pantalla.blit(texto_menu, boton_menu)
-
-    return menu
 
     pygame.display.update()
-
 
 
 
